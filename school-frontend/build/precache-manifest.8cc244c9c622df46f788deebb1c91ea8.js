f41598910b2e44c09dcb1185c8ded88c0ccf6d8c
Mustafa <maxusmusti@gmail.com>
2021-08-23 02:00:06 -0400

    Production build

---
+++ school-frontend/build/precache-manifest.8cc244c9c622df46f788deebb1c91ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbf8c789240c54eeafda7a152f0e55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4420025888ff4e954b05",
+    "url": "/static/css/main.a64ff00c.chunk.css"
+  },
+  {
+    "revision": "e980499e16beef204156",
+    "url": "/static/js/2.4cf5a0b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cf5a0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4420025888ff4e954b05",
+    "url": "/static/js/main.0d39aae5.chunk.js"
+  },
+  {
+    "revision": "be6de73e2e4068a6c7f7",
+    "url": "/static/js/runtime-main.239a2706.js"
+  }
+]);
